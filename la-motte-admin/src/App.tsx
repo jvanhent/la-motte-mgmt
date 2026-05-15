6c0c65900333cc1afa146b7b969f4fbd89f0a4e2
Johnny Vanhentenryk <johnny.vanhentenryk@ixor.be>
2026-05-15 21:13:30 +0200

    add customer assets

--- la-motte-admin/src/App.tsx
+++ la-motte-admin/src/App.tsx
@@ -1,33 +1,19 @@
 
 import React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
-import { createClient } from "@supabase/supabase-js";
-import { supabaseDataProvider } from "ra-supabase";
+import dataProvider from "./data-provider-config.ts";
 import AssetTypeList from "./pages/asset-types/asset-type-list.tsx";
 import AssetList from "./pages/assets/asset-list.tsx";
+import CustomerList from "./pages/customers/customer-list.tsx";
+import CustomerShow from "./pages/customers/customer-show.tsx";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY, {
-    db: {
-        schema: "la_motte_mgmt"
-    }
-});
-
-const dataProvider = supabaseDataProvider({
-    supabaseClient: supabase,
-    instanceUrl: SUPABASE_URL,
-    apiKey: SUPABASE_API_KEY,
-    schema: () => "la_motte_mgmt",
-});
 
 export default function App() {
     return (
         <Admin dataProvider={dataProvider}>
             <Resource name="asset_types" list={AssetTypeList} edit={EditGuesser} show={ShowGuesser} />
             <Resource name="assets" list={AssetList} edit={EditGuesser} show={ShowGuesser} />
-            <Resource name="customers" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+            <Resource name="customers" list={CustomerList} edit={EditGuesser} show={CustomerShow} />
         </Admin>
     );
 }
