7b7f89859c2158f50bc8f6ebfe0df8ee54608575
Johnny Vanhentenryk <johnny.vanhentenryk@ixor.be>
2026-05-17 01:56:11 +0200

    NoSsr

--- la-motte-admin/lib/supabase.ts
+++ la-motte-admin/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl : string = process.env.SUPABASE_URL!
-const supabaseApiKey : string = process.env.SUPABASE_API_KEY!
-const supabaseSchema : string = process.env.SUPABASE_SCHEMA!
+const supabaseUrl : string = process.env.SUPABASE_URL || 'https://server.url'
+const supabaseApiKey : string = process.env.SUPABASE_API_KEY || 'key'
+const supabaseSchema : string = process.env.SUPABASE_SCHEMA || 'public'
 
 const supabase = createClient(supabaseUrl, supabaseApiKey, {
     db: {
